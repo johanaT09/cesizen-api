eec0c48fc5e6d549667c482264494c6fb4c91e11
Azure DevOps Pipeline <johana@example.com>
2026-04-24 11:02:37 +0200

    Fix : resolution conflit #48

--- app/Repositories/Informations/CategorieActiviteRepository.php
+++ app/Repositories/Informations/CategorieActiviteRepository.php
@@ -6,11 +6,8 @@ use App\Models\CategorieActivite;
 
 class CategorieActiviteRepository
 {
-    // Dans CategorieActiviteRepository.php
     public function getAllCategories()
     {
-        // Attention : ne retourne qu'une liste de libellés sans ID
-        // return CategorieActivite::select('id_categorie', 'libelle_categorie')->get();
         return CategorieActivite::all(['libelle_categorie']);
     }
 }
