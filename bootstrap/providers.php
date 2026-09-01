c2dcb592f7de28cd1ee01e2ba0c208bf9de0e405
Johana Terrier <johana.terrier@apollossc.com>
2026-09-01 09:51:35 +0200

    Fix : Ajout de la supervision Sentry AB#132

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use App\Providers\SentryServiceProvider;
 
 return [
     AppServiceProvider::class,
-    SentryServiceProvider::class,
 ];
--- config/sentry.php
+++ config/sentry.php
@@ -1,13 +1,5 @@
 <?php
 
 return [
-
-    'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
-
-    // Options du client Guzzle sous-jacent
-    'guzzle_server_variable' => false,
-    
-    // Définir les options de transport
-    'transport' => \Sentry\Transport\HttpTransport::class,
-
+    'dsn' => env('SENTRY_LARAVEL_DSN'),
 ];
